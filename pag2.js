f55f18dfff9ff0d4c083ac01efd63728813bc8dc
luis916 <69111765+luis916@users.noreply.github.com>
2021-02-25 07:35:40 -0300

    Update pag2.js

--- pag2.js
+++ pag2.js
@@ -3,14 +3,18 @@ const volver = document.getElementById('volver');
 const ciclo = document.getElementById('ciclo');
 setInterval(function(){
     segundos.textContent = segundos.textContent-1;
-    if(segundos.textContent == 0){segundos.textContent = 120;ciclo.textContent =parseInt(ciclo.textContent)+1;}
+    if(segundos.textContent == 0)
+    {
+        segundos.textContent = 120;
+        ciclo.textContent = parseInt(ciclo.textContent)+1;
+        
+    }speechSynthesis.speak(new SpeechSynthesisUtterance(segundos.textContent))
 },1000);
-
+speechSynthesis.speak(new SpeechSynthesisUtterance(segundos.textContent))
 volver.addEventListener('click',function(){
    window.onbeforeunload = function(e) {
         return 'Perdera todos los cambios';
       };
 })
-
 
 
