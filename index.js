3a5fd0da63f7bba69e382474728db56b0437d463
luis916 <69111765+luis916@users.noreply.github.com>
2021-02-25 06:44:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//export const prueba = "Prueba";
+
+const comenzar = document.getElementById('comenzar');
+
+comenzar.addEventListener('click',function(){
+    window.location.replace('pag2.html');
+});
+
