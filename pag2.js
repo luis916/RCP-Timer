a6e09a242c94dfa0cd46acaf3db743a1c8227384
luis916 <69111765+luis916@users.noreply.github.com>
2021-02-25 09:36:01 -0300

    Update pag2.js

--- pag2.js
+++ pag2.js
@@ -1,20 +1,30 @@
+
 const segundos = document.getElementById('segundos');
 const volver = document.getElementById('volver');
 const ciclo = document.getElementById('ciclo');
+const dropdownContent = document.getElementById('dropdown-content');
+const dropdownBtn = document.querySelector('.dropdown-btn');
+dropdownContent.style.display = 'none';
+dropdownContent.style.transitionDuration = '10s';
+dropdownBtn.addEventListener('click',function(){
+    dropdownContent.style.display = 'block';
+    setTimeout(function(){
+        dropdownContent.style.transitionDuration = '10s';
+        dropdownContent.style.display = 'none';
+    },4000);
+});
 setInterval(function(){
-    segundos.textContent = segundos.textContent-1;
-    if(segundos.textContent == 0)
-    {
-        segundos.textContent = 120;
-        ciclo.textContent = parseInt(ciclo.textContent)+1;
-        
-    }speechSynthesis.speak(new SpeechSynthesisUtterance(segundos.textContent))
+    if(segundos.textContent>10){
+        segundos.textContent = segundos.textContent-1;
+    }else
+    if(segundos.textContent<=10){
+        segundos.textContent = segundos.textContent-1;
+       // speechSynthesis.speak(new SpeechSynthesisUtterance(segundos.textContent))
+    }
 },1000);
-speechSynthesis.speak(new SpeechSynthesisUtterance(segundos.textContent))
+
 volver.addEventListener('click',function(){
    window.onbeforeunload = function(e) {
         return 'Perdera todos los cambios';
       };
 })
-
-
