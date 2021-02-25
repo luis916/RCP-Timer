309b0a790a62ce7878f71e41b7aa1a34be764155
luis916 <69111765+luis916@users.noreply.github.com>
2021-02-25 07:10:40 -0300

    Update pag2.js

--- pag2.js
+++ pag2.js
@@ -1,10 +1,16 @@
-
 const segundos = document.getElementById('segundos');
-
+const volver = document.getElementById('volver');
+const ciclo = document.getElementById('ciclo');
 setInterval(function(){
     segundos.textContent = segundos.textContent-1;
+    if(segundos.textContent == 0){segundos.textContent = 120;ciclo.textContent =parseInt(ciclo.textContent)+1;}
 },1000);
 
+volver.addEventListener('click',function(){
+   window.onbeforeunload = function(e) {
+        return 'Perdera todos los cambios';
+      };
+})
 
 
 
