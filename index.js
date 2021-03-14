402f740f9a60b3387ef16f94043c09bd963ad4b5
luis916 <69111765+luis916@users.noreply.github.com>
2021-03-14 19:19:20 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-//export const prueba = "Prueba";
-
-const comenzar = document.getElementById('comenzar');
-
-comenzar.addEventListener('click',function(){
-    window.location.replace('pag2.html');
-});
-
